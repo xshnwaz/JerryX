8ed56ef8b50382386f3a306c900533ff27c9b42f
xshnwaz <xshnwaz@gmail.com>
2026-04-10 17:30:32 +0530

    Update config.py
    Signed-off-by: xshnwaz <xshnwaz@gmail.com>

--- config.py
+++ config.py
@@ -50,8 +50,8 @@ GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/RessoUpdates")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/DOCTOR_CHATTING_GROUP")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/as3lt")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/as3lt")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", None))
@@ -92,10 +92,10 @@ confirmer = {}
 
 
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://envs.sh/WJ-.jpg"
+    "START_IMG_URL", "https://files.catbox.moe/7scejy.png"
 )
 PING_IMG_URL = getenv(
-    "PING_IMG_URL", "https://envs.sh/WJ-.jpg"
+    "PING_IMG_URL", "https://files.catbox.moe/7scejy.png"
 )
 PLAYLIST_IMG_URL = "https://envs.sh/_B5.jpg"
 STATS_IMG_URL = "https://envs.sh/WJ-.jpg"
