9c1a9de204cfc16b50fbcc04310636981cb41b33
xshnwaz <xshnwaz@gmail.com>
2026-04-10 18:09:53 +0530

    Update config.py
    Signed-off-by: xshnwaz <xshnwaz@gmail.com>

--- config.py
+++ config.py
@@ -34,7 +34,7 @@ LOGGER_ID = int(getenv("LOGGER_ID", None))
 
 
 # Get this value from @FallenxBot on Telegram by /id
-OWNER_ID = int(getenv("OWNER_ID", 1356469075))
+OWNER_ID = int(getenv("OWNER_ID", 8206476526))
 
 ## Fill these variables if you're deploying on heroku.
 # Your heroku app name
