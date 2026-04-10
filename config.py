0794493b6fdd699ac52f6fdf794fa155364270b1
xshnwaz <xshnwaz@gmail.com>
2026-04-10 18:05:35 +0530

    Update config.py
    Signed-off-by: xshnwaz <xshnwaz@gmail.com>

--- config.py
+++ config.py
@@ -20,9 +20,10 @@ MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 600))
 
-API_URL = getenv("API_URL", 'https://api.thequickearn.xyz') #youtube song url
-API_KEY = getenv("API_KEY", None) # youtube song api key, generate free key or buy paid plan from panel.thequickearn.xyz
+from os import getenv
 
+API_URL = getenv("API_URL", "https://api.nexgenbots.xyz")
+API_KEY = getenv("API_KEY", None)  # Get your API key from https://console.nexgenbots.xyz
 
 
 # Set this to true if you want post ads automatically
